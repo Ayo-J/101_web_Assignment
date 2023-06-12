82a024b069b5f9d7ebe86e7abbf180a31e5b42af
Janu Ayodya <118840840+januayodya@users.noreply.github.com>
2023-06-12 06:02:54 +0530

    6/12/23
    base  to life

---
+++ life.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    
+    [...document.querySelectorAll('.first-column')].map(column => {
+    column.style.setProperty('--animation', 'slide');
+    column.style.setProperty('height', '200%');
+    column.innerHTML = column.innerHTML + column.innerHTML;
+ }
+ ); 
+     
+
